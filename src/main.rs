46d4b27b05855d2a1581fc8f47fb1a42a53554b1
ecto0310 <ecto0310@gmail.com>
2020-06-14 15:13:14 +0900

    Add function to load data

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,36 @@
-fn main() {}
+use serde::Deserialize;
+use std::fs::File;
+use std::io::BufReader;
+
+/// Key and token structure for calling API
+#[derive(Debug, Deserialize)]
+struct Token {
+  consumer_key: String,
+  consumer_secret: String,
+  oauth_token: String,
+  oauth_token_secret: String,
+}
+
+/// Schedule structure
+#[derive(Debug, Deserialize)]
+struct Schedule {
+  date: String,
+  message: String,
+  result: bool,
+}
+
+/// Configuration structure
+#[derive(Debug, Deserialize)]
+struct Data {
+  token: Token,
+  schedule: Vec<Schedule>,
+}
+
+fn main() {
+  // Load schedule
+  let file = File::open("data.json").unwrap();
+  let reader = BufReader::new(file);
+  let data: Data = serde_json::from_reader(reader).unwrap();
+  let _token = data.token;
+  let _schedule = data.schedule;
+}
