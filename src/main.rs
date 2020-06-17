a7613d94e9e93a355add03dd7c58829e3ed72979
ecto0310 <ecto0310@gmail.com>
2020-06-18 00:52:34 +0900

    Add function to post tweet

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use chrono::prelude::{DateTime, Local, TimeZone};
 use chrono::Duration;
+use percent_encoding::{utf8_percent_encode, AsciiSet};
+use reqwest::header::HeaderMap;
 use serde::Deserialize;
 use std::fs::File;
 use std::io::BufReader;
@@ -7,6 +9,14 @@ use std::thread;
 
 /// Time format
 const FORMAT: &str = "%Y-%m-%d %H:%M:%S%.3f";
+
+/// Conversion rule
+const FRAGMENT: &AsciiSet = &percent_encoding::NON_ALPHANUMERIC
+  .remove(b'*')
+  .remove(b'-')
+  .remove(b'.')
+  .remove(b'_')
+  .remove(b'~');
 
 /// Key and token structure for calling API
 #[derive(Debug, Deserialize)]
@@ -32,12 +42,48 @@ struct Data {
   schedule: Vec<Schedule>,
 }
 
+/// Response structure
+#[derive(Debug, Deserialize)]
+struct Response {
+  id: i64,
+}
+
+/// Get OAuth for the request
+///
+/// * endpoint - access destination
+/// * token - access token
+/// * params - parameter
+fn get_request_oauth(_endpoint: &str, _token: &Token, _params: Vec<(&str, &str)>) -> String {
+  "".to_string()
+}
+
 /// Post tweet
 ///
 /// * message - message string
 /// * token - access token
-fn post_tweet(_message: &str, _token: &Token) -> i64 {
-  0
+async fn post_tweet(message: &str, token: &Token) -> Result<i64, reqwest::Error> {
+  let endpoint = "https://api.twitter.com/1.1/statuses/update.json";
+  let mut params: Vec<(&str, &str)> = Vec::new();
+  params.push(("status", message));
+  let mut headers = HeaderMap::new();
+  headers.insert(
+    "Authorization",
+    get_request_oauth(endpoint, token, params).parse().unwrap(),
+  );
+  headers.insert(
+    "Content-Type",
+    "application/x-www-form-urlencoded".parse().unwrap(),
+  );
+
+  let res: Response = reqwest::Client::new()
+    .post(endpoint)
+    .headers(headers)
+    .body(format!("status={}", utf8_percent_encode(message, FRAGMENT)))
+    .send()
+    .await?
+    .json()
+    .await?;
+  Ok(res.id)
 }
 
 /// Delete tweet
@@ -59,8 +105,8 @@ fn post_reply(_id: i64, _message: &str, _token: &Token) {}
 /// * token - access token
 /// * delete - delete setting
 /// * result - result setting
-fn tweet(message: &str, token: &Token, delete: bool, result: bool) -> DateTime<Local> {
-  let id = post_tweet(message, token);
+async fn tweet(message: &str, token: &Token, delete: bool, result: bool) -> DateTime<Local> {
+  let id = post_tweet(message, token).await.unwrap();
   if delete {
     delete_tweet(id, token);
   }
@@ -76,7 +122,7 @@ fn tweet(message: &str, token: &Token, delete: bool, result: bool) -> DateTime<L
 ///
 /// * schedule - schedule data
 /// * token - access token
-fn time_tweet(schedule: Schedule, token: &Token) {
+async fn time_tweet(schedule: Schedule, token: &Token) {
   let target_time = Local.datetime_from_str(&schedule.date, FORMAT).unwrap();
   let test_target_time: DateTime<Local> = target_time - Duration::seconds(1);
   thread::sleep(
@@ -85,7 +131,7 @@ fn time_tweet(schedule: Schedule, token: &Token) {
       .to_std()
       .unwrap(),
   );
-  let test_date = tweet("test", token, true, false);
+  let test_date = tweet("test", token, true, false).await;
   let diff = test_target_time.signed_duration_since(test_date);
   thread::sleep(
     (target_time + diff)
@@ -93,10 +139,11 @@ fn time_tweet(schedule: Schedule, token: &Token) {
       .to_std()
       .unwrap(),
   );
-  tweet(&schedule.message, token, false, schedule.result);
+  tweet(&schedule.message, token, false, schedule.result).await;
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), ()> {
   // Load schedule
   let file = File::open("data.json").unwrap();
   let reader = BufReader::new(file);
@@ -112,6 +159,7 @@ fn main() {
       .to_std()
       .unwrap();
     thread::sleep(wait);
-    time_tweet(i, &token);
+    time_tweet(i, &token).await;
   }
+  Ok(())
 }
